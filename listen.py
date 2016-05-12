f76461128a28884e550a91815892ebc4cf2d5ea5
Sebastian Florian <rebardesigndk@gmail.com>
2016-05-12 16:55:16 +0200

    feature(listen): mac considerations

--- listen.py
+++ listen.py
@@ -1,10 +1,14 @@
 import serial
 import webbrowser
 import os
-browserExe = "chromium-browse"
+# Set correct browser to open and close
+# browserExe = "chromium-browse"
+browserExe = "Google Chrome"
 
-myPort = "/dev/ttyACM0"
+#myPort = "/dev/ttyACM0"
+myPort = "/dev/cu.usbmodem1421"
 baudRate = 9600
+# python serial will need to be downloaded and python setup.py install 
 ser = serial.Serial(myPort, baudRate)
 
 previousMode = 0
@@ -26,5 +30,6 @@ while 1:
             # if turning off   
             if not mode:
                 print("Mode is off: %s" % mode)
-                os.system("pkill "+browserExe)
+                # os.system("pkill " + browserExe)
+		os.system("kill -9 " + browserExe) 
             previousMode = mode
